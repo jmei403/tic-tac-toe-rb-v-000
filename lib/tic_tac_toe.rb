a3e5cc8d4af88fd6a2b19e51c217d875915d0dbd
Jason Mei <jason.mei403@gmail.com>
2018-04-30 22:42:47 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -97,3 +97,15 @@ def play(board)
     turn(board)
   end
 end
+
+def play(board)
+  until over?(board) == true
+    turn(board)
+  end
+
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+end
